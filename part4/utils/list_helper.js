da198a8a0bbd9301311a4a7f8a2411f81cb72c9c
Eduardo Castro <eduardo.alencar.castro@gmail.com>
2024-01-07 11:43:06 -0300

    updates

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
-const total_likes = (blogs) => {};
+const total_likes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  if (blogs.length === 1) {
+    likes = blogs[0].likes;
+    return likes;
+  } else {
+    let sum = 0;
+    for (n = 0; n < blogs.length; n++) {
+      sum = sum + blogs[n].likes;
+    }
+    return sum;
+  }
+};
 
 module.exports = {
   dummy,
