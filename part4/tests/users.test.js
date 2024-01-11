80037dc9e82a948e827d1b88d2f6b8b119fe716d
Eduardo Castro <eduardo.alencar.castro@gmail.com>
2024-01-11 14:11:03 -0300

    4.16done

--- part4/tests/users.test.js
+++ part4/tests/users.test.js
@@ -1,21 +1,12 @@
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
-
-//...
+const listHelper = require("../utils/list_helper");
+const mongoose = require("mongoose");
+const helper = require("./helper");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
 
 describe("when there is initially one user in db", () => {
-  beforeEach(async () => {
-    await User.deleteMany({});
-
-    const passwordHash = await bcrypt.hash("sekret", 10);
-    const user = new User({ username: "root", passwordHash });
-
-    await user.save();
-  });
-
   test("creation succeeds with a fresh username", async () => {
-    const usersAtStart = await helper.usersInDb();
-
     const newUser = {
       username: "mluukkai",
       name: "Matti Luukkainen",
@@ -27,11 +18,21 @@ describe("when there is initially one user in db", () => {
       .send(newUser)
       .expect(201)
       .expect("Content-Type", /application\/json/);
-
-    const usersAtEnd = await helper.usersInDb();
-    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
-
-    const usernames = usersAtEnd.map((u) => u.username);
-    expect(usernames).toContain(newUser.username);
   });
+
+  test("Post with 2 characters in username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "ml",
+      name: "Matti Luukkainen",
+      password: "salainen",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
 });
